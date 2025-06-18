6afde051c45010a2cea855a6fafaf3aaeec18eb6
eli <lykkh78@gmail.com>
2025-06-18 18:18:02 +0300

    adding player class

---
+++ Entities/Players/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelligenceInvestigation.Entities.Players
+{
+    public class Player
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string NickName { get; private set; }
+        public int Stage;
+        public Player(string name, string nickName, int stage = 0)
+        {
+            Name = name;
+            NickName = nickName;
+            Stage = stage;
+        }
+        public Player(int id, string name,
+                      string nickName,
+                      int stage = 0)
+            : this(name, nickName, stage)
+        {
+            Id = id;
+        }
+    }
+}
